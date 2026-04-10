022ee19087c7ac164e45997f060039335141a67d
amr_khaled <160667190+AmrTeemann12@users.noreply.github.com>
2026-04-10 10:24:33 +0200

    add dialog for adding new books, add delete button. added delete button for each card to delete the book object from the array, then re-render the library element. attaching the event handler on creation. added the new-book dialog which opens popup form, for registering a new book to the library. added some basic validation for the new-book form. changed the read property to status with three possible values 'finished', 'in progress', and 'not started'. assigned specific book properties' values to be displayed - in new then-appended html elements - instead fo just iterating across all the object properties. made book status button, that its text value represents the book current status which is toggled on button click.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const myLibrary = [];
+const libraryElem = document.querySelector('.library');
+const addBookBtn = document.querySelector('.add-book');
+const closeDialog = document.querySelector('#close-dialog');
+const dialog = document.querySelector('#new-book');
+const form = document.querySelector('#book-form');
+const errorDiv = document.querySelector('.error-message');
 
-function Book(title, author, pages, read, score) {
+function Book(title, author, pages, status) {
     if(!new.target){
         throw Error ('use "new" keyword for constructor invocation')
     }
@@ -8,20 +14,16 @@ function Book(title, author, pages, read, score) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.score = score;
+    this.status = status;
     this.id = crypto.randomUUID();
 }
 
-const libraryElem = document.querySelector('.library')
-
 function addBookToLibrary (book){
-
     let repeated = false;
     myLibrary.forEach(savedBook => {
         if(book.title === savedBook.title &&
             book.author === savedBook.author){
-            console.log('the book already exists');
+            console.warn('the book already exists');
             repeated = true;
         }
     })
@@ -34,29 +36,126 @@ function addBookToLibrary (book){
 }
 
 function displayBooks (library){
-    libraryElem.innerHTML = ""
+    libraryElem.innerHTML = "";
+
     library.forEach(book => {
         const card = document.createElement('div');
-        for(let key in book){
-            if (key === "id") continue;
-            const elem = document.createElement('div')
-            elem.textContent = `${key[0].toUpperCase()+key.slice(1)}: ${book[key]}`
-            card.appendChild(elem);
-        }
+        const titleElem = document.createElement('span');
+        const authorElem = document.createElement('span');
+        const pagesElem = document.createElement('span');
+        const statusElem = document.createElement('button');
+        const removeBtn = document.createElement('button');
+
+        titleElem.textContent = `"${book.title}"`;
+        authorElem.textContent = book.author;
+        pagesElem.textContent = book.pages? `${book.pages} pages`: '';
+        statusElem.textContent = book.status;
+        removeBtn.textContent = "Delete Book";
+
+        card.append(titleElem, authorElem, pagesElem, statusElem, removeBtn)
         
+        titleElem.classList.add('title-span');
+        authorElem.classList.add('author-span');
+        pagesElem.classList.add('pages-span');
+        statusElem.classList.add('status-btn');
+        statusElem.classList.add(`${book.status.split(' ').join('-')}`)
+        removeBtn.classList.add('remove-btn');
+
+
+        statusElem.addEventListener('click', () => {
+            let newStatus;
+
+            switch(book.status){
+                case 'not started':
+                    newStatus = 'in progress';
+                break;
+                case 'in progress':
+                    newStatus = 'finished';
+                break;
+                case 'finished':
+                    newStatus = 'not started';
+                break;
+            }
+            book.status = newStatus;
+            statusElem.textContent = newStatus;
+            statusElem.classList.remove('not-started', 'in-progress', 'finished');
+            statusElem.classList.add(`${newStatus.split(' ').join('-')}`)
+        })
+
+        removeBtn.addEventListener('click', (e) => {
+            const cardId = e.target.parentElement.dataset.id;
+            removeBook(cardId);
+        })
+
         libraryElem.appendChild(card);
         card.classList.add('card')
+        card.dataset.id = book.id;
     });
 }
 
-function removeBook (book){
-    const index = myLibrary.findIndex(savedBook => savedBook.id === book.id);
+function removeBook (id){
+    const index = myLibrary.findIndex(book => book.id === id);
     if (index !== -1){
         myLibrary.splice(index, 1);
 
         const bookRemoved = new CustomEvent('update');
-        libraryElem.dispatchEvent(bookRemoved)
+        libraryElem.dispatchEvent(bookRemoved);
     }
 }
 
 libraryElem.addEventListener('update', () => displayBooks(myLibrary))
+
+closeDialog.addEventListener('click', resetForm)
+
+addBookBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const initialLibrary = myLibrary.length;
+    
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        return;
+    }
+
+    const newTitle = document.querySelector('#title').value;
+    const newAuthor = document.querySelector('#author').value;
+    const newPages = document.querySelector('#pages').value;
+    const newStatus = document.querySelector('input[name="status"]:checked').value;
+
+    const newBook = new Book(newTitle, newAuthor, newPages, newStatus);
+    addBookToLibrary(newBook);
+
+    if(myLibrary.length === initialLibrary){
+        showError(`${newTitle} by ${newAuthor} already exists`);
+        return;
+    }
+
+    resetForm();
+    dialog.close();
+})
+
+function resetForm (){
+    errorDiv.innerHTML = '';
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#pages').value = '';
+    const defaultRadio = document.querySelector('#not-started');
+    defaultRadio.checked = true;   
+}
+
+function showError (message){
+    errorDiv.innerHTML = '';
+    
+    const errorText = document.createElement('p')
+    errorText.textContent = message;
+    errorDiv.appendChild(errorText);
+}
+
+const book1 = new Book('Tea Types and Methods', 'Tea Sage', 212, 'finished');
+const book2 = new Book('The Cat Way For Doing Things', 'R.G.Cat', 123, 'not started');
+const book3 = new Book('The Secret Of Drinking Two Cups Of Tea At The Same Time', 'A.T.Sage', 74, 'in progress');
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+displayBooks(myLibrary);
